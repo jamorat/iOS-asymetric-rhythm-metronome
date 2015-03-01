74c1f34a06c52b860a58de6d82ab63e8092492ed
jackamoratis <johnwamoratis@gmail.com>
2015-02-28 23:40:14 -0500

    posting updated code

---
+++ ViewController.m
@@ -0,0 +1,298 @@
+//  ViewController.m
+//  Variable Metrognome / Asymetric Rhythm
+//
+//  Created by Brian Sleeper and Jack Amoratis on 5/17/14.
+//  Released into the public domain.
+//  We think this is awesome software, but in all humility, there's nothing
+//  highly secret going on here. A timer, some sounds being played,
+//  some decimals being converted into fractions. That represents the entire
+//  functionality of the program. All you need to do is pop in some buttons, labels,
+//  and a table into a view and you will have your own working variable
+//  metronome. If you can make use of this code, then feel free
+//  to fork it, or just cut and paste. No warranty is given, and we make
+//  no representation that this code is suitable for a paricular purpose.
+//  Attribution appreciated but not required. This code is public domain. Enjoy!
+//  - Jack Amoratis and Brian Sleeper
+
+#import "MetronomeAppDelegate.h"
+#import "MetronomeViewController.h"
+# undef M_PI_2
+# define M_PI_2     1.57079632679489661923
+
+@interface MetronomeViewController ()
+@property (nonatomic, retain) IBOutlet UITableView *timingList;
+@property (nonatomic, assign) int counter;
+@property float currentItemMSTop;
+@property float quarterNoteMS;
+@property float eighthNoteMS;
+@property float halfNoteMS;
+@property float dottedHalfNoteMS;
+@property (nonatomic, assign) int itemCounter;
+@property (nonatomic) NSMutableArray *noteIcons;
+@end
+
+@implementation MetronomeViewController
+
+MetronomeAppDelegate *metronomeAppDelegate;
+-(void)viewDidDisappear:(BOOL)animated{
+    [self startStopTimer:YES];
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    _plusButton.titleLabel.font = [UIFont systemFontOfSize:20];
+    _timeItems = [NSMutableArray arrayWithObjects: @"1", @"1", @"2", nil];
+    _noteIcons = [NSMutableArray arrayWithObjects:@"eighth-note.png",@"quarter-note.png",@"half-note.png",@"dotted-half-note.png",nil];
+    [_timingList reloadData];
+    self.stepValue = 1.0f;
+    
+    // Set the initial value to prevent any weird inconsistencies.
+    self.lastQuestionStep = (self.bpmSlideController.value) / self.stepValue*100;
+    [self.editButton setTitle:@"Edit" forState:UIControlStateNormal];
+    self.bpmSlideController.value = 320;
+    [self updateSlider];
+}
+
+- (NSUInteger)supportedInterfaceOrientations {
+    return  UIInterfaceOrientationMaskAll;
+}
+
+-(BOOL)shouldAutorotate{
+    return YES;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return TRUE;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    [self startStopTimer:NO];
+    NSLog(@"%@",@"Show alert low memory");
+}
+
+- (IBAction)pushedGoButton:(id)sender {
+    [self startStopTimer:NO];
+}
+
+- (void) startStopTimer:(BOOL)specialStopFlag{
+    
+    if ([[_goButton currentTitle] isEqualToString:@"Start"] && specialStopFlag != YES) {
+        NSLog(@"Started");
+        [_goButton setTitle:@"Stop" forState:UIControlStateNormal];
+        [_goButton setTitleColor:[UIColor redColor] forState:UIControlStateNormal];
+        
+        NSURL *soundURL1 = [[NSBundle mainBundle] URLForResource:@"tone" withExtension:@"aiff"];        AudioServicesCreateSystemSoundID(CFBridgingRetain(soundURL1), &sound2);
+        [self metroAct];
+        
+    }else{
+        NSLog(@"Stopped");
+        [_goButton setTitle:@"Start" forState:UIControlStateNormal];
+        [_goButton setTitleColor:[UIColor greenColor] forState:UIControlStateNormal];
+        [_bpmTimer invalidate];
+        _bpmTimer = nil;
+    }
+}
+
+- (void)metroAct
+{
+    if ([_timeItems count] < 1) {
+        [self startStopTimer:YES];
+    }
+    
+    //will crash if stop while last item on the list is highlighted
+    //at that point the counter is above the count of time items - 1
+    if (_counter > [_timeItems count]-1){
+        _counter = 0;
+    }
+    NSLog(@"Current counter: %i", _counter);
+    //get time of current cell
+    NSString *whichSound = [_timeItems objectAtIndex:_counter];
+    [_timingList scrollToRowAtIndexPath:[NSIndexPath indexPathForRow:_counter inSection:0] atScrollPosition:UITableViewScrollPositionBottom animated:YES];
+    
+    //CURRENT CELL NOTE VALUE
+    if ([whichSound isEqualToString:@"0"]){
+        _currentItemMSTop = 60/_eighthNoteMS;
+    }else if([whichSound isEqualToString:@"1"]){
+        _currentItemMSTop = 60/_quarterNoteMS;
+    }else if([whichSound isEqualToString:@"2"]){
+        _currentItemMSTop = 60/_halfNoteMS;
+    }else if([whichSound isEqualToString:@"3"]){
+        _currentItemMSTop = 60/_dottedHalfNoteMS;
+    }
+    _bpmTimer = [NSTimer scheduledTimerWithTimeInterval:(_currentItemMSTop) target:self selector:@selector(soundPlayMethod) userInfo:nil repeats:NO];
+}
+
+- (void)soundPlayMethod{
+    AudioServicesPlaySystemSound(sound2);
+    if (_counter > ([_timeItems count]-1)){
+        _counter = 0;
+    }
+    [_timingList reloadData];
+    [self metroAct];
+    _counter++;
+}
+
+- (IBAction)bpmSlideControllerAction:(UISlider *)sender {
+    [self updateSlider];
+}
+
+-(void)updateSlider{
+    float newStep = roundf((self.bpmSlideController.value) / self.stepValue);
+    
+    
+    // Convert "steps" back to the context of the sliders values.
+    self.bpmSlideController.value = newStep * self.stepValue;
+    
+    _eighthNoteMS = (float)self.bpmSlideController.value;
+    _quarterNoteMS = (float)self.bpmSlideController.value/2;
+    _halfNoteMS = (float)self.bpmSlideController.value/3;
+    _dottedHalfNoteMS = (float)self.bpmSlideController.value/4;
+    
+    self.bpmDisplay.text = [NSString stringWithFormat:@"%d %@",[self intFromFloat:_eighthNoteMS],[self fractionEnding:_eighthNoteMS]];
+    self.bigDisplay.text = [NSString stringWithFormat:@"%d",[self intFromFloat:_eighthNoteMS]];
+    self.quarterNote.text = [NSString stringWithFormat:@"%d %@",[self intFromFloat:_quarterNoteMS],[self fractionEnding:_quarterNoteMS]];
+    self.halfNote.text = [NSString stringWithFormat:@"%d %@",[self intFromFloat:_halfNoteMS], [self fractionEnding:_halfNoteMS]];
+    self.dottedHalfNote.text = [NSString stringWithFormat:@"%d %@",[self intFromFloat:_dottedHalfNoteMS], [self fractionEnding:_dottedHalfNoteMS]];
+}
+
+- (NSString *)fractionEnding:(float)param{
+    NSString *whichFraction = [self generateFormattedNumber:param];
+    if ([whichFraction isEqualToString:@"0"] || [whichFraction isEqualToString:@"00"]) {
+        return @"";
+    } else if([whichFraction isEqualToString:@"25"]) {
+        return @"¼";
+    } else if ([whichFraction isEqualToString:@"33"]) {
+        return @"⅓";
+    } else if ([whichFraction isEqualToString:@"50"]) {
+        return @"½";
+    } else if ([whichFraction isEqualToString:@"66" ]) {
+        return @"⅔";
+    } else {
+        return @"";
+    }
+}
+
+- (NSString *)generateFormattedNumber:(float)param{
+    int newnew = 100 * (param-floor(param));
+    return [NSString stringWithFormat:@"%d", newnew];
+}
+
+- (int)intFromFloat:(float)param{
+    int newnew = floor(param);
+    return newnew;
+}
+
+-(NSInteger) tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return self.timeItems.count;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return 80;
+}
+
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return YES;
+}
+
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)sourceIndexPath toIndexPath:(NSIndexPath *)destinationIndexPath{
+    
+}
+
+- (UITableViewCell *) tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    NSString * ident = @"aCell";
+    UITableViewCell * cell = [tableView dequeueReusableCellWithIdentifier:nil];
+    if (cell ==nil){
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:ident];
+        NSString * txt = [self.timeItems objectAtIndex:indexPath.row];
+        NSLog(@"Current txt: %@ row: %li",txt, (long)indexPath.row);
+        
+        if ([txt isEqualToString: @"0"]) {
+            cell.textLabel.font =[UIFont fontWithName:@"MusiSync" size:70];
+            cell.textLabel.text = @"  e";
+        }else if ([txt isEqualToString: @"1"]) {
+            cell.textLabel.font =[UIFont fontWithName:@"MusiSync" size:70];
+            cell.textLabel.text = @"  q";
+        }else if ([txt isEqualToString: @"2"]) {
+            cell.textLabel.font =[UIFont fontWithName:@"MusiSync" size:70];
+            cell.textLabel.text = @"  j";
+        }else if ([txt isEqualToString: @"3"]) {
+            cell.textLabel.font =[UIFont fontWithName:@"MusiSync" size:70];
+            cell.textLabel.text = @"  h";
+        }
+        
+        if (indexPath.row == _counter-1){
+            [cell setBackgroundColor:[UIColor orangeColor]];
+            [cell setHighlighted:YES];
+        }
+        [[cell imageView] setHidden:NO];
+    }
+    
+    return cell;
+}
+
+
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    //handles taps on the cells to change value
+    UITableViewCell *cell = [tableView cellForRowAtIndexPath:indexPath];
+    if ([[_timeItems objectAtIndex:indexPath.row] isEqualToString:@"0"]) {
+        [_timeItems replaceObjectAtIndex:indexPath.row withObject:[NSString stringWithFormat:@"1"]];
+        cell.imageView.image = [UIImage imageNamed:_noteIcons[0]];
+    } else if ([[_timeItems objectAtIndex:indexPath.row] isEqualToString:@"1"]) {
+        [_timeItems replaceObjectAtIndex:indexPath.row withObject:[NSString stringWithFormat:@"2"]];
+        cell.textLabel.text = @"♩";
+        //cell.imageView.image = [UIImage imageNamed:_noteIcons[1]];
+    } else if ([[_timeItems objectAtIndex:indexPath.row] isEqualToString:@"2"]) {
+        [_timeItems replaceObjectAtIndex:indexPath.row withObject:[NSString stringWithFormat:@"3"]];
+        cell.imageView.image = [UIImage imageNamed:_noteIcons[2]];
+    } else if ([[_timeItems objectAtIndex:indexPath.row] isEqualToString:@"3"]) {
+        [_timeItems replaceObjectAtIndex:indexPath.row withObject:[NSString stringWithFormat:@"0"]];
+        cell.imageView.image = [UIImage imageNamed:_noteIcons[3]];
+    }
+    [_timingList reloadData];
+}
+
+-(void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath{
+    if (editingStyle == UITableViewCellEditingStyleDelete){
+        
+        if (indexPath.row < [_timeItems count]){
+            
+            /* First remove this object from the source */
+            [_timeItems removeObjectAtIndex:indexPath.row];
+            
+            /* Then remove the associated cell from the Table View */
+            [tableView deleteRowsAtIndexPaths:[NSArray arrayWithObject:indexPath]
+                             withRowAnimation:UITableViewRowAnimationLeft];
+        }
+    }
+}
+
+- (IBAction)addItem:(UIButton *)sender {
+    [_timeItems addObject:@"1"];
+    [_timingList reloadData];
+    [_timingList scrollToRowAtIndexPath:[NSIndexPath indexPathForRow:[_timeItems count]-1 inSection:0] atScrollPosition:UITableViewScrollPositionBottom animated:NO];
+    [_timingList flashScrollIndicators];
+}
+
+- (IBAction)editButtonPush:(id)sender {
+    if ([[sender currentTitle] isEqualToString:@"Edit"]){
+        [sender setTitle:@"Done" forState:UIControlStateNormal];
+        _timingList.editing=YES;
+    } else {
+        [sender setTitle:@"Edit" forState:UIControlStateNormal];
+        _timingList.editing=NO;
+    }
+}
+
+- (IBAction)clearButtonPressed:(UIButton *)sender {
+    _timeItems = [NSMutableArray arrayWithObjects: @"0", nil];
+    [_timingList reloadData];
+    [self startStopTimer:YES];
+}
+@end
